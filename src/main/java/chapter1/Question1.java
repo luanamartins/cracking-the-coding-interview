174310af4af44f85b0e3dd34da839ee224bb6ed8
Luana Martins <lms7@cin.ufpe.br>
2017-04-21 16:19:34 -0300

    Add resolution for question 1 from chapter 1

--- src/main/java/chapter1/Question1.java
+++ src/main/java/chapter1/Question1.java
@@ -1,5 +1,29 @@
 package chapter1;
 
+/**
+ * 
+ * Is Unique: Implement an algorithm to determine if a string has all unique
+ * characters. What if you cannot use additional data structures?
+ * 
+ */
+
 public class Question1 {
 
+	public static boolean isUnique(String input){
+		boolean hasUniqueCharacters = true;
+		for(int i = 0; i < input.length() && hasUniqueCharacters; i++){
+			for (int j = i + 1; j < input.length(); j++) {
+				if(input.charAt(i) == input.charAt(j)){
+					hasUniqueCharacters = false;
+				}
+			}
+		}
+		return hasUniqueCharacters;
+	}
+	
+	public static void main(String[] args) {
+		String input = "ab";
+		System.out.println(isUnique(input));
+		
+	}
 }
