3de35f437706d1c34cc029c496a1aba5f252a769
Luana Martins <lms7@cin.ufpe.br>
2017-04-29 11:47:12 -0300

    Add LinkedListNode object

---
+++ src/main/java/chapter2/LinkedListNode.java
@@ -0,0 +1,28 @@
+package chapter2;
+
+public class LinkedListNode<T> {
+
+	public T value;
+	public LinkedListNode<T> next;
+
+	public LinkedListNode(T value) {
+		this.value = value;
+	}
+
+	public void add(T value) {
+		if (this.next == null) {
+			this.next = new LinkedListNode<T>(value);
+		} else {
+			this.next.add(value);
+		}
+	}
+
+	public void add(LinkedListNode<T> node) {
+		if (this.next == null) {
+			this.next = node;
+		} else {
+			this.next.add(node);
+		}
+	}
+
+}
