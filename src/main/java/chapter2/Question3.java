de630c2654956d3559cfc6592e9304d156cf7471
Luana Martins <lms7@cin.ufpe.br>
2017-04-29 10:07:37 -0300

    Add solution for question 3 of chapter 2

---
+++ src/main/java/chapter2/Question3.java
@@ -0,0 +1,53 @@
+package chapter2;
+
+/**
+ * 
+ * Delete Middle Node: Implement an algorithm to delete a node in the middle
+ * (i.e., any node but the first and last node, not necessarily the exact
+ * middle) of a singly linked list, given only access to that node. EXAMPLE
+ * 
+ * Input: 
+ * the node c from the linked list a -> b -> c -> d -> e -> f 
+ * 
+ * Result:
+ * nothing is returned, but the new linked list looks like a -> b -> d -> e -> f
+ * 
+ */
+
+public class Question3 {
+
+	public static void deleteMiddleNode(LinkedListNode<Character> middle) {
+		if (middle != null && middle.next != null) {
+			middle.value = middle.next.value;
+			middle.next = middle.next.next;
+		}
+	}
+
+	public static void main(String[] args) {
+		LinkedListNode<Character> list = new LinkedListNode<>('a');
+		list.add('b');
+		list.add('c');
+		list.add('d');
+		list.add('e');
+
+		LinkedListNode<Character> pointer = list;
+		boolean isMiddle = false;
+		int counter = 1;
+
+		while (!isMiddle && pointer != null) {
+			pointer = pointer.next;
+			if (counter == 2) {
+				isMiddle = true;
+			}
+			counter++;
+		}
+
+		deleteMiddleNode(pointer);
+
+		LinkedListNode<Character> current = list;
+		while (current != null) {
+			System.out.print(current.value + " ");
+			current = current.next;
+		}
+	}
+}
