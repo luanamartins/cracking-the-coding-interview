242b05f4eecd0601cacb0359affae4802f96da3b
Luana Martins <lms7@cin.ufpe.br>
2017-08-05 11:18:02 -0300

    Format the contents of some files

--- src/main/java/chapter2/Question5.java
+++ src/main/java/chapter2/Question5.java
@@ -14,8 +14,10 @@ import java.util.LinkedList;
  * FOLLOW UP
  * 
  * Suppose the digits are stored in forward order. Repeat the above problem.
- * EXAMPLE Input: (6 -> 1 -> 7) + (2 -> 9 -> 5).That is, 617 + 295. Output: 9 ->
- * 1 -> 2. That is, 912.
+ * EXAMPLE 
+ * Input: (6 -> 1 -> 7) + (2 -> 9 -> 5). That is, 617 + 295. 
+ * Output: 9 -> 1 -> 2. That is, 912.
+ * 
  */
 
 public class Question5 {
